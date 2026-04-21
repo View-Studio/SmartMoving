c0e5e1a452645a32f7a5905458a96131c2d8f252
Rudals <2153rud@gmail.com>
2026-04-21 20:47:40 +0900

    chore: 기존 구현 전체 삭제 — 리서치 기반 완전 재작성 준비

--- src/client/java/choco/ratel/smartmoving/client/SmartmovingClient.java
+++ src/client/java/choco/ratel/smartmoving/client/SmartmovingClient.java
@@ -1,34 +1,10 @@
 package choco.ratel.smartmoving.client;
 
-import choco.ratel.smartmoving.client.input.InputHandler;
-import choco.ratel.smartmoving.client.input.SmartMovingKeys;
-import choco.ratel.smartmoving.client.network.SmartMovingClientNetworking;
-import choco.ratel.smartmoving.client.render.SmartMovingHud;
-import choco.ratel.smartmoving.network.RemotePlayerManager;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
-import net.minecraft.client.MinecraftClient;
 
-public class SmartmovingClient implements ClientModInitializer {
+public class SmartMovingClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        SmartMovingKeys.register();
-        InputHandler.register();
-        SmartMovingClientNetworking.register();
-        SmartMovingHud.register();
-
-        // 매 틱 로컬 플레이어 상태를 서버로 전송
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if (client.player != null && client.getNetworkHandler() != null) {
-                SmartMovingClientNetworking.sendStateIfChanged(client.player);
-            }
-        });
-
-        // 서버 연결 해제 시 상태 초기화
-        ClientPlayConnectionEvents.DISCONNECT.register((handler, client) -> {
-            SmartMovingClientNetworking.resetPrevState();
-        });
     }
 }
--- src/main/java/choco/ratel/smartmoving/Smartmoving.java
+++ src/main/java/choco/ratel/smartmoving/Smartmoving.java
@@ -1,18 +1,12 @@
 package choco.ratel.smartmoving;
 
-import choco.ratel.smartmoving.config.ConfigManager;
-import choco.ratel.smartmoving.network.SmartMovingServerNetworking;
-import choco.ratel.smartmoving.state.SmartMovingAttachments;
 import net.fabricmc.api.ModInitializer;
 
-public class Smartmoving implements ModInitializer {
+public class SmartMoving implements ModInitializer {
 
     public static final String MOD_ID = "smartmoving";
 
     @Override
     public void onInitialize() {
-        ConfigManager.load();
-        SmartMovingAttachments.register();
-        SmartMovingServerNetworking.register();
     }
 }
